3ba8130197d0049f75ad774e83e2b96cb4028406
village22 <village22@naver.com>
2024-01-24 16:07:02 +0900

    Swagger 활성화 파일

--- SpinrgStudy/src/main/java/org/example/spinrgstudy/config/SwaggerConfig.java
+++ SpinrgStudy/src/main/java/org/example/spinrgstudy/config/SwaggerConfig.java
@@ -3,7 +3,6 @@ package org.example.spinrgstudy.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
 @Configuration
 @EnableWebMvc
 public class SwaggerConfig {
