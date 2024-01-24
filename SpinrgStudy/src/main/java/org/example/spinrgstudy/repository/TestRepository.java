e98a2dc237262f6f47562c80048e23b5a0729465
village22 <village22@naver.com>
2024-01-24 16:58:14 +0900

    test파일 삭제

--- SpinrgStudy/src/main/java/org/example/spinrgstudy/repository/TestRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.spinrgstudy.repository;
-
-import org.example.spinrgstudy.entity.TestEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestRepository extends JpaRepository<TestEntity, Long> {
-}
