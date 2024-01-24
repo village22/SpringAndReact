7e4d736e2b021416c68f1abb422d3f8d96803d0b
village22 <village22@naver.com>
2024-01-24 12:45:41 +0900

    Spirng 프로젝트 생성

---
+++ SpinrgStudy/src/main/java/org/example/spinrgstudy/SpinrgStudyApplication.java
@@ -0,0 +1,13 @@
+package org.example.spinrgstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpinrgStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpinrgStudyApplication.class, args);
+    }
+
+}
---
+++ SpinrgStudy/src/test/java/org/example/spinrgstudy/SpinrgStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spinrgstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpinrgStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
